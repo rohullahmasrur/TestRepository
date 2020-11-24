255ef0a2dd2ba094dafad0df559c8e9207069eb2
rohullahmasrur <74948467+rohullahmasrur@users.noreply.github.com>
2020-11-23 21:34:40 -0800

    added a comment

--- TestSolution/TestProjectWindowsForm/Program.cs
+++ TestSolution/TestProjectWindowsForm/Program.cs
@@ -14,6 +14,7 @@ namespace TestProjectWindowsForm
         [STAThread]
         static void Main()
         {
+            // comment
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
